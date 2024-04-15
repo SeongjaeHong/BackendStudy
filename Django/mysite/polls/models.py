b36db6d35cbb08f923fc284807dded5cce94eb84
Seongjae Hong <sjhong310@gmail.com>
2024-04-16 00:57:26 +0900

    Bug fix

--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -4,7 +4,8 @@ import datetime
 
 class Question(models.Model):
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
