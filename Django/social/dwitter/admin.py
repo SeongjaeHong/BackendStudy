71769d8fbb7a7ee635829a6d021bdf837237f1bc
Seongjae Hong <sjhong310@gmail.com>
2024-04-20 20:13:33 +0900

    Change admin page

--- Django/social/dwitter/admin.py
+++ Django/social/dwitter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+    
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
