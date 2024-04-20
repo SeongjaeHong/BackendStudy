9da4ab4f058b258e42458e9915f4c44f1001135d
Seongjae Hong <sjhong310@gmail.com>
2024-04-20 22:40:35 +0900

    Add follow/unfollow func

--- Django/social/dwitter/views.py
+++ Django/social/dwitter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, create_profile
 
 def dashboard(request):
     return render(request, 'base.html')
@@ -9,5 +9,17 @@ def profile_list(request):
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
 
 def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        create_profile(request.user)
+        
     profile = Profile.objects.get(pk=pk)
+    if request.method == 'POST':
+        cur_user_profile = request.user.profile
+        data = request.POST
+        action = data.get('follow')
+        if action == 'follow':
+            cur_user_profile.follows.add(profile)
+        elif action == 'unfollow':
+            cur_user_profile.follows.remove(profile)
+        cur_user_profile.save()
     return render(request, 'dwitter/profile.html', {'profile': profile})
