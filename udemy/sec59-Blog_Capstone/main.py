87529b451d1a865decabcbd945e093f177a5f0d7
Seongjae Hong <sjhong310@gmail.com>
2025-01-12 00:22:01 +0900

    Correct css path and bootstrap src

--- udemy/sec59-Blog_Capstone/main.py
+++ udemy/sec59-Blog_Capstone/main.py
@@ -3,8 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def main():
     return render_template('index.html')
+
+@app.route('/post.html')
+def page_post():
+    return render_template('post.html')
+
+@app.route('/about.html')
+def page_about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def page_contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
