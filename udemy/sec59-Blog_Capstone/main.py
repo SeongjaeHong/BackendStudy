728bd5ff4de837c621272f330be1709d1e067fb1
Seongjae Hong <sjhong310@gmail.com>
2025-01-12 01:18:22 +0900

    Render an individual page

--- udemy/sec59-Blog_Capstone/main.py
+++ udemy/sec59-Blog_Capstone/main.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
+response = requests.get('https://api.npoint.io/495f53c32892edd20f6e')
+posts = response.json()
 
 @app.route('/')
 @app.route('/index.html')
 def main():
-    response = requests.get('https://api.npoint.io/495f53c32892edd20f6e')
-    posts = response.json()
     return render_template('index.html', posts=posts)
 
-@app.route('/post.html')
+@app.route('/post.html', methods=['GET'])
 def page_post():
-    return render_template('post.html')
+    post_id = int(request.args['post_id']) - 1
+    return render_template('post.html',
+                           post=posts[post_id])
 
 @app.route('/about.html')
 def page_about():
