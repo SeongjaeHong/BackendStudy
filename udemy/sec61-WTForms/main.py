2efd2f2570f2c7fb6c82804bf889e34c28a453f5
Seongjae Hong <sjhong310@gmail.com>
2025-01-12 17:06:14 +0900

    Inheritance

--- udemy/sec61-WTForms/main.py
+++ udemy/sec61-WTForms/main.py
@@ -23,10 +23,14 @@ def home():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    myform = MyForm()
-    if myform.validate_on_submit():
-        return redirect(request.url)
-    return render_template('login.html', form=myform)
+    login_form = MyForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
