adcf848502f49a917b0093e6fb4d5a8940fde43b
Seongjae Hong <sjhong310@gmail.com>
2025-01-14 02:45:40 +0900

    method

---
+++ java/method/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Add: " + add(5,6));
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
