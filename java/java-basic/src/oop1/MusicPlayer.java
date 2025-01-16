46b566ecbd99a055e4297fd57035a2479c11622f
Seongjae Hong <sjhong310@gmail.com>
2025-01-17 01:32:39 +0900

    생성자

---
+++ java/java-basic/src/oop1/MusicPlayer.java
@@ -0,0 +1,11 @@
+package oop1;
+
+public class MusicPlayer {
+    int volume = 0;
+    boolean isOn = false;
+
+    MusicPlayer(int volume, boolean isOn) {
+        this.volume = volume;
+        this.isOn = isOn;
+    }
+}
--- java/java-basic/src/oop1/MusicPlayerMain1.java
+++ java/java-basic/src/oop1/MusicPlayerMain1.java
@@ -2,31 +2,8 @@ package oop1;
 
 public class MusicPlayerMain1 {
     public static void main(String[] args) {
-        int volume = 0;
-        boolean isOn = false;
-
-        // Turn on music player
-        isOn = true;
-        System.out.println("Start music player.");
-
-        // Volume up
-        volume++;
-        System.out.println("Volume: " + volume);
-
-        // Volume down
-        volume--;
-        System.out.println("Volue: " + volume);
-
-        // Volume status
-        if (isOn) {
-            System.out.println("Music player On, Volume: " + volume);
-        }
-        else {
-            System.out.println("Music player Off");
-        }
-
-        // Off music player
-        isOn = false;
-        System.out.println("Finish music player");
+        MusicPlayer player = new MusicPlayer(3,false);
+        System.out.println(player.volume);
+        System.out.println(player.isOn);
     }
 }
