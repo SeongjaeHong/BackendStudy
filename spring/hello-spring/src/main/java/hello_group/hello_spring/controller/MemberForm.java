2f2c4c356f2229ae726575febe633aff9fc8e3df
Seongjae Hong <sjhong310@gmail.com>
2025-02-01 02:06:58 +0900

    Member resister

---
+++ spring/hello-spring/src/main/java/hello_group/hello_spring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello_group.hello_spring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getMemberName() {
+        return name;
+    }
+
+    public void setMemberName(String name) {
+        this.name = name;
+    }
+}
