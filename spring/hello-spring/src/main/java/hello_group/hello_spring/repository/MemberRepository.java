1f836ff5af2c920b7a8e57795e0d809d9fd9ab23
Seongjae Hong <sjhong310@gmail.com>
2025-02-01 00:16:23 +0900

    Member init

---
+++ spring/hello-spring/src/main/java/hello_group/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello_group.hello_spring.repository;
+
+import hello_group.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findall();
+}
