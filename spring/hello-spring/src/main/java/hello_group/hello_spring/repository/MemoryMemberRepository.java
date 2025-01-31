2328d36d60bd7e005509c291961f7e5138e1e735
Seongjae Hong <sjhong310@gmail.com>
2025-02-01 01:14:53 +0900

    Add Spring stereotype

--- spring/hello-spring/src/main/java/hello_group/hello_spring/repository/MemoryMemberRepository.java
+++ spring/hello-spring/src/main/java/hello_group/hello_spring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello_group.hello_spring.repository;
 
 import hello_group.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
