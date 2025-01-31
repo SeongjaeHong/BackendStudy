eccf2d702c25e82a8bf986a83e5dfdba8ebacbbc
Seongjae Hong <sjhong310@gmail.com>
2025-02-01 00:34:29 +0900

    Init service

---
+++ spring/hello-spring/src/main/java/hello_group/hello_spring/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello_group.hello_spring.service;
+
+import hello_group.hello_spring.domain.Member;
+import hello_group.hello_spring.repository.MemberRepository;
+import hello_group.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateMemberDuplication(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateMemberDuplication(Member member) {
+        /*
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("Member already exists.");
+        });
+        */
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("Member already exists.");
+                        });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findall();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
