8ade31fb39427c34206ac2f7e9c6b73763729ca3
Seongjae Hong <sjhong310@gmail.com>
2025-02-09 18:32:33 +0900

    Member service

---
+++ spring/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)  // readonly 설정하면 성능 향상
+@RequiredArgsConstructor  // final 변수는 생성자로 설정. 생성자가 한 개인 경우, spring에서 자동 autowired.
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        // Exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
