36a1989383dc72498feff58fb557b93623de04c6
Seongjae Hong <sjhong310@gmail.com>
2025-02-09 19:07:03 +0900

    Item repository and service

--- spring/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+    public void addStock(int quantity) {
+        stockQuantity += quantity;
+    }
+
+    public void subtractStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("잔량이 부족합니다.");
+        }
+        stockQuantity -= quantity;
+    }
 }
