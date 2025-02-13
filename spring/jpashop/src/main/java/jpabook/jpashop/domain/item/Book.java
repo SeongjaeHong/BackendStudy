54c9803aa271411f9efb1335e7ed31ececc6e63c
Seongjae Hong <sjhong310@gmail.com>
2025-02-14 01:04:56 +0900

    add boo item

--- spring/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -2,14 +2,23 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jpabook.jpashop.controller.BookForm;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @DiscriminatorValue("B")
-@Getter @Setter
+@Getter
 public class Book extends Item {
 
-    private String author;
-    private String isbn;
+    private final String author;
+    private final String isbn;
+
+    public Book(BookForm bookForm) {
+        this.setName(bookForm.getName());
+        this.setPrice(bookForm.getPrice());
+        this.setStockQuantity(bookForm.getStockQuantity());
+        this.author = bookForm.getAuthor();
+        this.isbn = bookForm.getIsbn();
+    }
 }
