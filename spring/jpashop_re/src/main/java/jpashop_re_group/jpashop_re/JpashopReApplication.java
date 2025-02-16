27d5e26bc689db44ebfb3590319a302c3dbec59b
Seongjae Hong <sjhong310@gmail.com>
2025-02-16 21:39:34 +0900

    init

---
+++ spring/jpashop_re/src/main/java/jpashop_re_group/jpashop_re/JpashopReApplication.java
@@ -0,0 +1,13 @@
+package jpashop_re_group.jpashop_re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopReApplication.class, args);
+	}
+
+}
---
+++ spring/jpashop_re/src/test/java/jpashop_re_group/jpashop_re/JpashopReApplicationTests.java
@@ -0,0 +1,13 @@
+package jpashop_re_group.jpashop_re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
