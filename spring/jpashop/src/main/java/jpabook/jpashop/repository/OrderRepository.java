3c92d00673630ee59f538be82dfe15a2a50ccfb5
Seongjae Hong <sjhong310@gmail.com>
2025-02-16 19:10:01 +0900

    Order repository

--- spring/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,21 +1,24 @@
 package jpabook.jpashop.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -92,4 +95,6 @@ public class OrderRepository {
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
         return query.getResultList();
     }
+
 }
+
