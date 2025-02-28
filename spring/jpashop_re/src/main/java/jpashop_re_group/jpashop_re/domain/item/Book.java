3b64867edb4fc028efd2bc11d5629b9af865e60d
Seongjae Hong <sjhong310@gmail.com>
2025-03-01 00:52:10 +0900

    Add setAttribute

--- spring/jpashop_re/src/main/java/jpashop_re_group/jpashop_re/domain/item/Book.java
+++ spring/jpashop_re/src/main/java/jpashop_re_group/jpashop_re/domain/item/Book.java
@@ -1,14 +1,20 @@
 package jpashop_re_group.jpashop_re.domain.item;
 
 import jakarta.persistence.Entity;
+import jpashop_re_group.jpashop_re.controller.BookForm;
 import jpashop_re_group.jpashop_re.domain.Product;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Setter @Getter
+@Getter
 public class Book extends Product {
 
     private String author;
     private String isbn;
+
+    public void setAttributes(BookForm form) {
+        super.setAttributes(form);
+        author = form.getAuthor();
+        isbn = form.getIsbn();
+    }
 }
