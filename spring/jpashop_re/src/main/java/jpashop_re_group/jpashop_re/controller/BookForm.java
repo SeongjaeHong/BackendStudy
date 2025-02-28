64ca6a7fc75f042c9269521b54fd1003e7a860e5
Seongjae Hong <sjhong310@gmail.com>
2025-03-01 01:41:27 +0900

    Change price type from int to long

--- spring/jpashop_re/src/main/java/jpashop_re_group/jpashop_re/controller/BookForm.java
+++ spring/jpashop_re/src/main/java/jpashop_re_group/jpashop_re/controller/BookForm.java
@@ -1,6 +1,6 @@
 package jpashop_re_group.jpashop_re.controller;
 
-import jpashop_re_group.jpashop_re.domain.Product;
+import jpashop_re_group.jpashop_re.domain.item.Book;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,19 +10,18 @@ public class BookForm {
     private Long id;
 
     private String name;
-    private int price;
+    private Long price;
     private int stockQuantity;
 
     private String author;
     private String isbn;
 
-    public void setAttribute(Product product) {
-        BookForm form = new BookForm();
-        form.setId(item.getId());
-        form.setName(item.getName());
-        form.setPrice(item.getPrice());
-        form.setStockQuantity(item.getStockQuantity());
-        form.setAuthor(item.getAuthor());
-        form.setIsbn(item.getIsbn());
+    public void setAttribute(Book book) {
+        id = book.getProductId();
+        name = book.getName();
+        price = book.getPrice();
+        stockQuantity = book.getQuantity();
+        author = book.getAuthor();
+        isbn = book.getIsbn();
     }
 }
