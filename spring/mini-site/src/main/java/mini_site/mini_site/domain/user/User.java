2f0b0c9629d1b74f4c6dfa56214922ec3bebe2a9
Seongjae Hong <sjhong310@gmail.com>
2025-03-20 00:38:30 +0900

    init user entity

--- spring/mini-site/src/main/java/mini_site/mini_site/domain/user/User.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/domain/user/User.java
@@ -1,7 +1,22 @@
 package mini_site.mini_site.domain.user;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class User {
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String password;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private String createdAt;
 }
