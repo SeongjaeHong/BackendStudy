941288eeea60c9606c6fdcb005216a7c6e6c9128
SeongjaeHong <sjhong310@gmail.com>
2025-03-22 16:32:28 +0900

    Add a constructor using billboard name

--- spring/mini-site/src/main/java/mini_site/mini_site/domain/billboard/Billboard.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/domain/billboard/Billboard.java
@@ -2,11 +2,14 @@ package mini_site.mini_site.domain.billboard;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
 public class Billboard {
     @Id @GeneratedValue
     @Column(name = "billboard_id")
@@ -17,4 +20,8 @@ public class Billboard {
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "billboard", cascade = CascadeType.ALL)
     private List<Post> posts = new ArrayList<>();
+
+    public Billboard(String name) {
+        this.name = name;
+    }
 }
