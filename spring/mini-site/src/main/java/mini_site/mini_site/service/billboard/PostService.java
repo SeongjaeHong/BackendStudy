451c3c370a421660698a876d7171727748dbbfab
SeongjaeHong <sjhong310@gmail.com>
2025-03-26 22:15:43 +0900

    Post Create, Search, Delete

--- spring/mini-site/src/main/java/mini_site/mini_site/service/billboard/PostService.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/service/billboard/PostService.java
@@ -1,8 +1,54 @@
 package mini_site.mini_site.service.billboard;
 
+import lombok.RequiredArgsConstructor;
+import mini_site.mini_site.domain.billboard.Billboard;
+import mini_site.mini_site.domain.billboard.Post;
+import mini_site.mini_site.domain.member.Member;
+import mini_site.mini_site.exception.PostException;
+import mini_site.mini_site.repository.billboard.PostRepository;
+import mini_site.mini_site.service.billboard.dto.request.PostRequest;
+import mini_site.mini_site.service.billboard.dto.response.PostResponse;
+import mini_site.mini_site.service.member.MemberService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostService {
+    private final PostRepository postRepository;
+    private final MemberService memberService;
+    private final BillboardService billboardService;
+
     // 글 작성
+    @Transactional
+    public ResponseEntity<PostResponse> writePost(PostRequest postRequest) {
+        Post post = getEntity(postRequest);
+        postRepository.save(post);
+        return ResponseEntity.ok(
+                new PostResponse(
+                        post.getId(),
+                        post.getMember().getId(),
+                        post.getBillboard().getId(),
+                        post.getContent()
+                )
+        );
+    }
+
+    // 글 삭제
+    @Transactional
+    public void deletePostById(Long postId) {
+        postRepository.deleteById(postId);
+    }
+
+    public Post findPostById(Long postId) {
+        return postRepository.findById(postId).orElseThrow(() -> new PostException("요청한 게시글을 찾을 수 없습니다."));
+    }
+
+    private Post getEntity(PostRequest postRequest) {
+        Member member = memberService.findMemberById(postRequest.memberId());
+        Billboard billboard = billboardService.findBillboardById(postRequest.billboardId());
+        return new Post(member, billboard, postRequest.content());
+    }
 }
---
+++ spring/mini-site/src/test/java/mini_site/mini_site/service/billboard/PostServiceTest.java
@@ -0,0 +1,69 @@
+package mini_site.mini_site.service.billboard;
+
+import mini_site.mini_site.domain.billboard.Billboard;
+import mini_site.mini_site.domain.billboard.Post;
+import mini_site.mini_site.domain.member.Member;
+import mini_site.mini_site.repository.billboard.BillboardRepository;
+import mini_site.mini_site.repository.billboard.PostRepository;
+import mini_site.mini_site.repository.member.MemberRepository;
+import mini_site.mini_site.service.billboard.dto.request.PostRequest;
+import mini_site.mini_site.service.billboard.dto.response.PostResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+    @Autowired PostService postService;
+    @Autowired PostRepository postRepository;
+    @Autowired MemberRepository memberRepository;
+    @Autowired BillboardRepository billboardRepository;
+
+    @Test
+    void writePost() {
+        // given
+        Member member = new Member("name", "pwd");
+        memberRepository.save(member);
+
+        Billboard billboard = new Billboard("name");
+        billboardRepository.save(billboard);
+
+        PostRequest postRequest = new PostRequest(member.getId(), billboard.getId(), "content");
+
+        // when
+        ResponseEntity<PostResponse> postResponseResponseEntity = postService.writePost(postRequest);
+        PostResponse postResponse = postResponseResponseEntity.getBody();
+
+        // then
+        assertEquals(200, postResponseResponseEntity.getStatusCode().value());
+        assertNotNull(postResponse);
+        Post foundPost = postService.findPostById(postResponse.postId());
+        assertEquals(postResponse.postId(), foundPost.getId());
+    }
+
+    @Test
+    void deletePost() {
+        // given
+        Member member = new Member("name", "pwd");
+        memberRepository.save(member);
+
+        Billboard billboard = new Billboard("name");
+        billboardRepository.save(billboard);
+
+        PostRequest postRequest = new PostRequest(member.getId(), billboard.getId(), "content");
+
+        // when
+        ResponseEntity<PostResponse> postResponseResponseEntity = postService.writePost(postRequest);
+        PostResponse postResponse = postResponseResponseEntity.getBody();
+        assertNotNull(postResponse);
+        postService.deletePostById(postResponse.postId());
+
+        // then
+        assertFalse(postRepository.existsById(postResponse.postId()));
+    }
+}
