23a53151f45ec87f5f36e81b2bb493f2c1a64aaf
SeongjaeHong <sjhong310@gmail.com>
2025-03-26 22:13:56 +0900

    Inherit JpaRepository

--- spring/mini-site/src/main/java/mini_site/mini_site/repository/billboard/PostRepository.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/repository/billboard/PostRepository.java
@@ -1,7 +1,7 @@
 package mini_site.mini_site.repository.billboard;
 
-import org.springframework.stereotype.Repository;
+import mini_site.mini_site.domain.billboard.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class PostRepository {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
