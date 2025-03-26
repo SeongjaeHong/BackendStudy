8402a7272157371a497038c6c85ab340a9f8a02c
SeongjaeHong <sjhong310@gmail.com>
2025-03-26 22:13:21 +0900

    Change user to member & create a constructor

--- spring/mini-site/src/main/java/mini_site/mini_site/domain/billboard/Post.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/domain/billboard/Post.java
@@ -1,21 +1,24 @@
 package mini_site.mini_site.domain.billboard;
 
 import jakarta.persistence.*;
-import mini_site.mini_site.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import mini_site.mini_site.domain.member.Member;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
 public class Post {
     @Id @GeneratedValue
     @Column(name = "post_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
-    private User user;
+    @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+    private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "billboard_id", referencedColumnName = "billboard_id")
@@ -29,4 +32,15 @@ public class Post {
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private String createdAt;
+
+    @Builder
+    public Post(
+            Member member,
+            Billboard billboard,
+            String content
+    ) {
+        this.member = member;
+        this.billboard = billboard;
+        this.content = content;
+    }
 }
