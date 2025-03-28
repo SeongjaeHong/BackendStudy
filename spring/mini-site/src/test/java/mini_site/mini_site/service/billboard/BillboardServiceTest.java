6c3cd74f399db0c437e76cdf3574d10ba6fe8516
SeongjaeHong <sjhong310@gmail.com>
2025-03-28 10:36:32 +0900

    findPostsByBillboardId

--- spring/mini-site/src/test/java/mini_site/mini_site/service/billboard/BillboardServiceTest.java
+++ spring/mini-site/src/test/java/mini_site/mini_site/service/billboard/BillboardServiceTest.java
@@ -1,14 +1,24 @@
 package mini_site.mini_site.service.billboard;
 
 import mini_site.mini_site.domain.billboard.Billboard;
+import mini_site.mini_site.domain.billboard.Post;
+import mini_site.mini_site.domain.member.Member;
 import mini_site.mini_site.domain.member.MemberLevel;
 import mini_site.mini_site.exception.BillboardException;
+import mini_site.mini_site.repository.billboard.BillboardRepository;
+import mini_site.mini_site.repository.billboard.PostRepository;
+import mini_site.mini_site.repository.member.MemberRepository;
+import mini_site.mini_site.service.billboard.dto.request.PostRequest;
+import mini_site.mini_site.service.billboard.dto.response.PostResponse;
 import mini_site.mini_site.service.member.dto.response.MemberResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +26,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class BillboardServiceTest {
     @Autowired BillboardService billboardService;
+    @Autowired PostService postService;
+
+    @Autowired BillboardRepository billboardRepository;
+    @Autowired PostRepository postRepository;
+    @Autowired MemberRepository memberRepository;
 
     @DisplayName("게시판을 등록한다.")
     @Test
@@ -43,5 +58,28 @@ class BillboardServiceTest {
         // then
         assertEquals(billboard, foundBillboard);
         assertThrows(BillboardException.class, () -> billboardService.findBillboardById(-1L));
+    }
+    @DisplayName("게시글을 조회한다.")
+    @Test
+    void findPostsByBillboardId() {
+        // given
+        Member member = new Member("user-1", "pwd");
+        memberRepository.save(member);
+
+        Billboard billboard = new Billboard("board-1");
+        billboardRepository.save(billboard);
+
+        ResponseEntity<PostResponse> res1 = postService.writePost(new PostRequest(member.getId(), billboard.getId(), "content-1"));
+        ResponseEntity<PostResponse> res2 = postService.writePost(new PostRequest(member.getId(), billboard.getId(), "content-2"));
+
+        // when
+        List<PostResponse> posts = billboardService.findPostsByBoardId(billboard.getId());
+
+        // then
+        assertEquals(2, posts.size());
+        assertAll(
+                () -> assertEquals(res1.getBody().postId(), posts.get(0).postId()),
+                () -> assertEquals(res2.getBody().postId(), posts.get(1).postId())
+        );
     }
 }
