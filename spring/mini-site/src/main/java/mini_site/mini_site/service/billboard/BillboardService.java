6702bb3c1d782ad53057a65356d6e75d5ca7fb2a
SeongjaeHong <sjhong310@gmail.com>
2025-04-07 18:34:58 +0900

    find all boards

--- spring/mini-site/src/main/java/mini_site/mini_site/service/billboard/BillboardService.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/service/billboard/BillboardService.java
@@ -35,6 +35,10 @@ public class BillboardService {
         return billboardRepository.findById(billBoardId).orElseThrow(() -> new BillboardException("요청한 게시판을 찾을 수 없습니다."));
     }
 
+    public List<Billboard> findAllBillboards() {
+        return billboardRepository.findAll();
+    }
+
     public List<PostResponse> findPostsByBoardId(Long billBoardId) {
         Billboard billboard = billboardRepository.findById(billBoardId).orElseThrow(() -> new BillboardException("요청한 게시판을 찾을 수 없습니다."));
 
