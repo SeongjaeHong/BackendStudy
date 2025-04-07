2c8d30f7f627576817daaf09f59c0fa3ac229ac2
SeongjaeHong <sjhong310@gmail.com>
2025-04-07 18:37:06 +0900

    init home page

--- spring/mini-site/src/main/java/mini_site/mini_site/controller/billboard/BillboardController.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/controller/billboard/BillboardController.java
@@ -1,9 +1,29 @@
 package mini_site.mini_site.controller.billboard;
 
+import lombok.RequiredArgsConstructor;
+import mini_site.mini_site.domain.billboard.Billboard;
+import mini_site.mini_site.service.billboard.BillboardService;
+import mini_site.mini_site.service.billboard.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class BillboardController {
-    // 게시판 페이지
-    // 글 페이지
+    private final BillboardService billboardService;
+
+    @GetMapping("/")
+    public String base(){
+        return "redirect:/ordinary";
+    }
+
+    @GetMapping("/ordinary")
+    public String home(Model model){
+        Billboard foundBoard = billboardService.findBillboardById(1L);
+        model.addAttribute("posts", foundBoard.getPosts());
+        model.addAttribute("boardName", "일반 게시판");
+        model.addAttribute("boards", billboardService.findAllBillboards());
+        return "home";
+    }
 }
