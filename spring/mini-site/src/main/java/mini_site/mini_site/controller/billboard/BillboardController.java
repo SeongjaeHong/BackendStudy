0b84269a33e54ab6a6ec33bbbb63087ddd4ef82d
SeongjaeHong <sjhong310@gmail.com>
2025-04-11 10:32:51 +0900

    add a write page

--- spring/mini-site/src/main/java/mini_site/mini_site/controller/billboard/BillboardController.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/controller/billboard/BillboardController.java
@@ -3,10 +3,12 @@ package mini_site.mini_site.controller.billboard;
 import lombok.RequiredArgsConstructor;
 import mini_site.mini_site.domain.billboard.Billboard;
 import mini_site.mini_site.service.billboard.BillboardService;
-import mini_site.mini_site.service.billboard.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,16 +16,30 @@ public class BillboardController {
     private final BillboardService billboardService;
 
     @GetMapping("/")
-    public String base(){
-        return "redirect:/ordinary";
-    }
-
-    @GetMapping("/ordinary")
-    public String home(Model model){
-        Billboard foundBoard = billboardService.findBillboardById(1L);
+    public String base(Model model){
+        String defaultBoard = "일반 게시판";
+        Billboard foundBoard = billboardService.findBillboardByName(defaultBoard);
         model.addAttribute("posts", foundBoard.getPosts());
-        model.addAttribute("boardName", "일반 게시판");
+        model.addAttribute("boardName", foundBoard.getName());
         model.addAttribute("boards", billboardService.findAllBillboards());
         return "home";
+    }
+
+    @GetMapping("/{boardName}")
+    public String home(@PathVariable String boardName, Model model){
+        Billboard foundBoard = billboardService.findBillboardByName(boardName);
+        model.addAttribute("posts", foundBoard.getPosts());
+        model.addAttribute("boardName", foundBoard.getName());
+        model.addAttribute("boards", billboardService.findAllBillboards());
+        return "home";
+    }
+
+    // 글 작성 페이지
+    @GetMapping("/write")
+    public String writePost(@RequestParam("board") String boardName, Model model) {        
+        Billboard foundBoard = billboardService.findBillboardByName(boardName);
+        model.addAttribute("boardName", foundBoard.getName());
+        model.addAttribute("boards", billboardService.findAllBillboards());
+        return "write";
     }
 }
