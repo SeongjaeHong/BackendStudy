38140604cb2e16e486ddc2dea927dfe7794a37cc
SeongjaeHong <sjhong310@gmail.com>
2025-04-15 18:31:18 +0900

    Use enum message

--- spring/mini-site/src/main/java/mini_site/mini_site/exception/MemberException.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/exception/MemberException.java
@@ -1,5 +1,7 @@
 package mini_site.mini_site.exception;
 
+import lombok.RequiredArgsConstructor;
+
 public class MemberException extends RuntimeException {
     public MemberException() {
         super();
@@ -7,6 +9,14 @@ public class MemberException extends RuntimeException {
 
     public MemberException(String message) {
         super(message);
+    }
+
+    public MemberException(MemberExceptionMessage message, String info) {
+        super(message + "(" + info + ")");
+    }
+
+    public MemberException(MemberExceptionMessage message, Long info) {
+        super(message + "(" + info + ")");
     }
 
     public MemberException(String message, Throwable cause) {
@@ -19,5 +29,14 @@ public class MemberException extends RuntimeException {
 
     protected MemberException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    @RequiredArgsConstructor
+    public enum MemberExceptionMessage {
+        USER_ID_NOT_FOUND("User id is not found."),
+        USER_LOGIN_ID_NOT_FOUND("User login-id is not found."),
+        USER_NAME_NOT_FOUND("User name is not found.");
+
+        private final String message;
     }
 }
--- spring/mini-site/src/main/java/mini_site/mini_site/service/member/MemberService.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/service/member/MemberService.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import mini_site.mini_site.domain.member.Member;
 import mini_site.mini_site.domain.member.MemberLevel;
 import mini_site.mini_site.exception.MemberException;
+import mini_site.mini_site.exception.MemberException.MemberExceptionMessage;
 import mini_site.mini_site.repository.member.MemberRepository;
 import mini_site.mini_site.service.member.dto.request.RegisterMemberRequest;
 import mini_site.mini_site.service.member.dto.response.MemberResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -16,12 +18,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    private Member getMemberById(Long memberId) {
-        return memberRepository.findById(memberId).orElseThrow(() -> new MemberException("요청한 사용자를 찾을 수 없습니다."));
+    public Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new MemberException(MemberExceptionMessage.USER_ID_NOT_FOUND, memberId));
     }
 
-    public Member findMemberById(Long memberId) {
-        return getMemberById(memberId);
+    public Member findMemberByLoginId(String loginId) {
+        return memberRepository.findAll().stream()
+                .filter(member -> member.getLoginId().equals(loginId))
+                .findFirst()
+                .orElseThrow(() -> new MemberException(MemberExceptionMessage.USER_LOGIN_ID_NOT_FOUND, loginId));
+    }
+
+    public Member findMemberByName(String memberName) {
+        return memberRepository.findAll().stream()
+                .filter(member -> member.getName().equals(memberName))
+                .findFirst()
+                .orElseThrow(() -> new MemberException(MemberExceptionMessage.USER_NAME_NOT_FOUND, memberName));
     }
 
     @Transactional
@@ -33,7 +45,7 @@ public class MemberService {
 
     @Transactional
     public MemberResponse grantMember(Long memberId) {
-        Member member = getMemberById(memberId);
+        Member member = findMemberById(memberId);
         member.setMemberLevel(MemberLevel.ADMIN);
 
         return new MemberResponse(member.getId(), member.getName(), member.getMemberLevel());
