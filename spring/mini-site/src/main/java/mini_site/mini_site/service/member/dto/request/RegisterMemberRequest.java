fefdb5f2da0a64bbf6743626208c8138e73df40f
SeongjaeHong <sjhong310@gmail.com>
2025-04-15 21:40:52 +0900

    add loginId field

--- spring/mini-site/src/main/java/mini_site/mini_site/service/member/dto/request/RegisterMemberRequest.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/service/member/dto/request/RegisterMemberRequest.java
@@ -3,11 +3,13 @@ package mini_site.mini_site.service.member.dto.request;
 import mini_site.mini_site.domain.member.Member;
 
 public record RegisterMemberRequest(
+        String loginId,
         String name,
         String password
 ) {
     public Member toEntity() {
         return Member.builder()
+                .loginId(loginId)
                 .name(name)
                 .password(password)
                 .build();
