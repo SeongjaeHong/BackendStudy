2bb921cbc7a9d68948c1e00eded2c0a9b9aacd6d
SeongjaeHong <sjhong310@gmail.com>
2025-04-17 13:05:04 +0900

    Change message to Korean

--- spring/mini-site/src/main/java/mini_site/mini_site/auth/message/ErrorMessage.java
+++ spring/mini-site/src/main/java/mini_site/mini_site/auth/message/ErrorMessage.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    LOGIN_FAILED(401, "Id or password is invalid."),
-    MEMBER_NOT_FOUND(404, "Id not exists.");
+    LOGIN_FAILED(401, "아이디 또는 비밀번호가 틀렸습니다."),
+    MEMBER_NOT_FOUND(404, "존재하지 않는 계정입니다.");
 
     private final int status;
     private final String msg;
